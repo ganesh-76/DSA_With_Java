9ce4e79616fa339d43714fdd2b6d97ccbac2ea5e
Ganesh Bansode <ganeshbansode8080@gmail.com>
2021-09-24 12:51:51 +0530

    New Approach

--- countSetBits.java
+++ countSetBits.java
@@ -1,18 +1,31 @@
 package com.example.test;
 
 public class countSetBits {
+    /*
     static int countingSetBits(int n){
         int res = 0;
         while(n>0){
             if (n%2 != 0){
                 res++;
             }
-            n = n/2;
-        }
-        return res;
+            n = n/2; // When we check right most bit we found that it is n/2 after eliminating n, bcoz of series.
+        }               // ex:-13= 0...1101, 0...0110, 0...0011, 0...0001, 0...0000
+        return res;     //             13        6         3         1
+    }
+     */
+    static int countingSetBits(int n){
+        int res = 0;
+        while(n != 0){
+            res = res + (n&1);
+            n = n >> 1; // When we check right most bit we found that it is n/2 after eliminating n, bcoz of series.
+        }               // ex:-13= 0...1101, 0...0110, 0...0011, 0...0001, 0...0000
+        return res;     //             13        6         3         1
     }
 
     public static void main(String[] args) {
-        System.out.println(countingSetBits(13));
+        int n = 13;
+        System.out.println(countingSetBits(n));
     }
+}
+
 }
