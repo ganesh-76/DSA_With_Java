d65de411804de0f994478b9de8beffd78afd87d4
Ganesh Bansode <ganeshbansode8080@gmail.com>
2021-10-04 21:38:39 +0530

    Updated

--- Try2.java
+++ Try2.java
@@ -1,7 +1,18 @@
 package com.example.test;
 
 public class Try2 {
+    static boolean checkKthBit(int n, int k) {
+        // Your code here
+        if (((n & (1 << k)) != 0) && n != 0){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Try your Programs here");
+        System.out.println(checkKthBit(1, 0));
     }
 }
